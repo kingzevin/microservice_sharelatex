1ca878c16f81d5cb9aa30aa2d86e5c793b4c608c
King.Zevin <king.zevin@gmail.com>
2019-12-05 19:25:00 +0800

    repo

--- services.js
+++ services.js
@@ -2,50 +2,50 @@ module.exports =
 
 [{
 	name: "web",
-	repo: "https://github.com/kingzevin/web-sharelatex.git",
+	repo: "https://github.com/kingzevin/web.git",
 	version: "master"
 }, {
 	name: "real-time",
-	repo: "https://github.com/kingzevin/real-time-sharelatex.git",
+	repo: "https://github.com/kingzevin/real-time.git",
 	version: "master"
 }, {
 	name: "document-updater",
-	repo: "https://github.com/kingzevin/document-updater-sharelatex.git",
+	repo: "https://github.com/kingzevin/document-updater.git",
 	version: "master"
 // }, {
 // 	name: "clsi",
-// 	repo: "https://github.com/kingzevin/clsi-sharelatex.git",
+// 	repo: "https://github.com/kingzevin/clsi.git",
 // 	version: "master"
 }, {
 	name: "filestore",
-	repo: "https://github.com/kingzevin/filestore-sharelatex.git",
+	repo: "https://github.com/kingzevin/filestore.git",
 	version: "master"
 }, {
 	name: "track-changes",
-	repo: "https://github.com/kingzevin/track-changes-sharelatex.git",
+	repo: "https://github.com/kingzevin/track-changes.git",
 	version: "master"
 }, {
 	name: "docstore",
-	repo: "https://github.com/kingzevin/docstore-sharelatex.git",
+	repo: "https://github.com/kingzevin/docstore.git",
 	version: "master"
 }, {
 	name: "chat",
-	repo: "https://github.com/kingzevin/chat-sharelatex.git",
+	repo: "https://github.com/kingzevin/chat.git",
 	version: "master"
 }, {
 	name: "tags",
-	repo: "https://github.com/kingzevin/tags-sharelatex.git",
+	repo: "https://github.com/kingzevin/tags.git",
 	version: "master"
 }, {
 	name: "spelling",
-	repo: "https://github.com/kingzevin/spelling-sharelatex.git",
+	repo: "https://github.com/kingzevin/spelling.git",
 	version: "master"
 }, {
 	name: "contacts",
-	repo: "https://github.com/kingzevin/contacts-sharelatex.git",
+	repo: "https://github.com/kingzevin/contacts.git",
 	version: "sk-update-mongojs"
 }, {
         name: "notifications",
-        repo: "https://github.com/kingzevin/notifications-sharelatex.git",
+        repo: "https://github.com/kingzevin/notifications.git",
         version: "master"
 }]

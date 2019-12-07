704d4c6794e5ec246cb5f5d38cb2b3543a5ac241
King.Zevin <king.zevin@gmail.com>
2019-12-07 17:40:58 +0800

    [clsi > filestore] clsi containerization done and filestore ready

---
+++ services-filestore.js
@@ -0,0 +1,7 @@
+module.exports = 
+
+[{
+	name: "filestore",
+	repo: "https://github.com/sharelatex/filestore-sharelatex.git",
+	version: "43b8a9093b8d1d394bbd6111ac19221faef5e05c"
+}]
--- services.js
+++ services.js
@@ -13,10 +13,6 @@ module.exports =
 	repo: "https://github.com/sharelatex/document-updater-sharelatex.git",
 	version: "0c3d8ca80b526fa13911e49372be396f6669396d"
 }, {
-	name: "filestore",
-	repo: "https://github.com/sharelatex/filestore-sharelatex.git",
-	version: "43b8a9093b8d1d394bbd6111ac19221faef5e05c"
-}, {
 	name: "track-changes",
 	repo: "https://github.com/sharelatex/track-changes-sharelatex.git",
 	version: "5e3e5e6cb7f94a205f5848e1b84a22c3e2778dce"

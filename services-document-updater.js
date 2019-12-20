730f0ae8dbdd5793da56ee86bf7ed827e39e3836
King.Zevin <king.zevin@gmail.com>
2019-12-20 15:18:23 +0800

    [containerize-document-updater]

---
+++ services-document-updater.js
@@ -0,0 +1,7 @@
+module.exports = 
+
+[{
+	name: "document-updater",
+	repo: "https://github.com/sharelatex/document-updater-sharelatex.git",
+	version: "0c3d8ca80b526fa13911e49372be396f6669396d"
+}]
--- services.js
+++ services.js
@@ -9,10 +9,6 @@ module.exports =
 	repo: "https://github.com/sharelatex/real-time-sharelatex.git",
 	version: "2060a2fdc5c6badfa92a717a6ff35dfe91ad9f20"
 }, {
-	name: "document-updater",
-	repo: "https://github.com/sharelatex/document-updater-sharelatex.git",
-	version: "0c3d8ca80b526fa13911e49372be396f6669396d"
-}, {
 	name: "track-changes",
 	repo: "https://github.com/sharelatex/track-changes-sharelatex.git",
 	version: "5e3e5e6cb7f94a205f5848e1b84a22c3e2778dce"

5f8b298bc577d39aea875112eaff30c344c06ffd
King.Zevin <king.zevin@gmail.com>
2019-12-20 16:47:07 +0800

    [containerize-web]

---
+++ services-web.js
@@ -0,0 +1,7 @@
+module.exports = 
+
+[{
+	name: "web",
+	repo: "https://github.com/sharelatex/web-sharelatex.git",
+	version: "fdeddd22"
+}]
--- services.js
+++ services.js
@@ -1,10 +1,6 @@
 module.exports = 
 
 [{
-	name: "web",
-	repo: "https://github.com/sharelatex/web-sharelatex.git",
-	version: "fdeddd22"
-}, {
 	name: "real-time",
 	repo: "https://github.com/sharelatex/real-time-sharelatex.git",
 	version: "2060a2fdc5c6badfa92a717a6ff35dfe91ad9f20"

1d9542dc38a52099b334ae51ce6f0e840aadbffe
King.Zevin <king.zevin@gmail.com>
2019-12-20 14:28:45 +0800

    [containerize-contacts]

---
+++ services-contacts.js
@@ -0,0 +1,7 @@
+module.exports = 
+
+[{
+	name: "contacts",
+	repo: "https://github.com/sharelatex/contacts-sharelatex.git",
+	version: "2a41cc7ea4c"
+}]
--- services.js
+++ services.js
@@ -16,8 +16,4 @@ module.exports =
 	name: "track-changes",
 	repo: "https://github.com/sharelatex/track-changes-sharelatex.git",
 	version: "5e3e5e6cb7f94a205f5848e1b84a22c3e2778dce"
-}, {
-	name: "contacts",
-	repo: "https://github.com/sharelatex/contacts-sharelatex.git",
-	version: "2a41cc7ea4c"
 }]

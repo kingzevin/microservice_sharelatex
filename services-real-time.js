ad4d84b33f43fb7416b476dd4ffb923d1d50f605
King.Zevin <king.zevin@gmail.com>
2019-12-20 17:06:04 +0800

    [containerize-real-time]

---
+++ services-real-time.js
@@ -0,0 +1,7 @@
+module.exports = 
+
+[{
+	name: "real-time",
+	repo: "https://github.com/sharelatex/real-time-sharelatex.git",
+	version: "2060a2fdc5c6badfa92a717a6ff35dfe91ad9f20"
+}]
--- services.js
+++ services.js
@@ -1,10 +1,6 @@
 module.exports = 
 
 [{
-	name: "real-time",
-	repo: "https://github.com/sharelatex/real-time-sharelatex.git",
-	version: "2060a2fdc5c6badfa92a717a6ff35dfe91ad9f20"
-}, {
 	name: "track-changes",
 	repo: "https://github.com/sharelatex/track-changes-sharelatex.git",
 	version: "5e3e5e6cb7f94a205f5848e1b84a22c3e2778dce"
